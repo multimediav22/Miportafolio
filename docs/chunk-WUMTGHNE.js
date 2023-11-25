882b35980ec80088a024907ea404a982514a7d48
multimediav22 <42384264+multimediav22@users.noreply.github.com>
2023-11-25 01:37:15 -0500

    Update href

---
+++ docs/chunk-WUMTGHNE.js
@@ -0,0 +1,48 @@
+import{Ba as s,Ka as e,La as t,Ma as r,Pa as n,S as l,Va as d,Wa as x,sa as u,ub as g,wb as m}from"./chunk-4POKWDE7.js";var y=a=>["w-full","rounded-md","overflow-hidden","bg-gray-200","transition-all","hover:bg-gray-300",a],f=(()=>{let i=class i{};i.\u0275fac=function(o){return new(o||i)},i.\u0275cmp=l({type:i,selectors:[["app-users-loaders"]],inputs:{cssClass:"cssClass"},standalone:!0,features:[d],decls:47,vars:3,consts:[[3,"ngClass"],[1,"p-4","md:p-6","relative"],[1,"absolute","inset-0","bg-gradient-to-r","from-gray-200","via-gray-300","to-gray-400","rounded-md","filter","blur-md"],[1,"relative","z-10","mx-auto","max-w-screen-lg"],[1,"bg-zinc-800","text-white","p-2","md:p-4","mb-4"],[1,"container","mx-auto"],[1,"text-2xl","md:text-3xl","lg:text-4xl","font-semibold","mb-2","md:mb-4","text-gray-400","leading-loose"],[1,"text-xl","md:text-3xl","lg:text-4xl","font-semibold","mb-2","md:mb-4","text-gray-800","leading-loose"],[1,"my-2","border-t","border-gray-400"],[1,"relative"],[1,"border-r-2","border-gray-400","absolute","h-full","top-0","hidden","md:block",2,"left","15px"],[1,"timeline-container","relative","flex","flex-col","md:flex-row","justify-center"],[1,"timeline-event","mb-8","md:mb-0","w-full","md:w-1/2","lg:w-1/3","transform","hover:translate-x-[5px]","transition-transform","md:mr-4"],[1,"timeline-badge","bg-gray-600"],[1,"text-white"],[1,"timeline-panel","bg-white","shadow-md","p-4","rounded-md"],[1,"text-lg","md:text-xl","font-semibold","text-gray-800","leading-tight"],[1,"text-gray-600","text-sm","mb-2"],[1,"text-gray-800","leading-loose"],[1,"timeline-event","w-full","md:w-1/2","lg:w-1/3","transform","hover:translate-x-[-5px]","transition-transform"]],template:function(o,p){o&1&&(e(0,"section",0)(1,"div",1),r(2,"div",2),e(3,"div",3)(4,"nav",4)(5,"div",5)(6,"div",6),n(7," INGENIERO MULTIMEDIA "),t()()(),e(8,"h2",7),n(9," Experiencia "),r(10,"hr",8),t(),e(11,"div",9),r(12,"div",10),e(13,"div",11)(14,"div",12)(15,"div",13)(16,"span",14),n(17,"2022"),t()(),e(18,"div",15)(19,"h3",16),n(20,"Desarrollador (junior)"),t(),e(21,"p",17),n(22,"LandianMeta"),t(),e(23,"p",18),n(24," Ocup\xE9 el puesto de desarrollador junior, centr\xE1ndome en el desarrollo de videojuegos y experiencias de realidad virtual utilizando el motor gr\xE1fico Unreal Engine, con un enfoque espec\xEDfico en el dise\xF1o y la interacci\xF3n de la interfaz de usuario (UI/UX) "),t()()(),e(25,"div",12)(26,"div",13)(27,"span",14),n(28,"2023"),t()(),e(29,"div",15)(30,"h3",16),n(31,"Desarrollo Backend Autodidacta"),t(),e(32,"p",17),n(33,"Enero - 2023"),t(),e(34,"p",18),n(35," Soy un desarrollador con s\xF3lidos conocimientos en Node.js y desarrollo del lado del servidor. Mi entusiasmo por el desaf\xEDo se refleja en mi incursi\xF3n autodidacta en PHP y mi inter\xE9s en Laravel 10. Mi experiencia en tecnolog\xEDas como Java, Spring Boot y Docker me impulsa a buscar entornos vers\xE1tiles. Busco expandir mis habilidades para ser un recurso valioso en el desarrollo de aplicaciones. "),t()()(),e(36,"div",19)(37,"div",13)(38,"span",14),n(39,"2023"),t()(),e(40,"div",15)(41,"h3",16),n(42,"Desarrollo Frontend Autodidacta (Junior)"),t(),e(43,"p",17),n(44,"2023"),t(),e(45,"p",18),n(46," Competente en Angular y con conocimientos s\xF3lidos en HTML, CSS (incluyendo Tailwind CSS y Bootstrap) y JavaScript, capaz de contribuir al desarrollo de interfaces de usuario atractivas y funcionales como desarrollador junior. Mi habilidad en maquetado y dise\xF1o web con Tailwind CSS y Bootstrap me permite crear sitios web modernos y responsivos que mejoran la experiencia del usuario. "),t()()()()()()()()),o&2&&s("ngClass",x(1,y,p.cssClass))},dependencies:[m,g],encapsulation:2});let a=i;return a})();var C=(()=>{let i=class i{};i.\u0275fac=function(o){return new(o||i)},i.\u0275cmp=l({type:i,selectors:[["ng-component"]],standalone:!0,features:[d],decls:16,vars:1,consts:[["lang","es"],["charset","UTF-8"],["name","viewport","content","width=device-width, initial-scale=1.0"],["rel","stylesheet","href","https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css","integrity","sha512-...","crossorigin","anonymous"],["rel","stylesheet","href","https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"],[1,"container"],[1,"section"],[1,"card","md:card-lg","lg:card-xl"],[1,"card-content"],[3,"cssClass"],[1,"text-gray-600","mt-4"]],template:function(o,p){o&1&&(e(0,"html",0)(1,"head"),r(2,"meta",1)(3,"meta",2)(4,"link",3)(5,"link",4),e(6,"title"),n(7,"Tu T\xEDtulo"),t()(),e(8,"body")(9,"div",5)(10,"section",6)(11,"div",7)(12,"div",8),r(13,"app-users-loaders",9),e(14,"p",10),n(15,"Backend - Frontend"),t()()()()()()()),o&2&&(u(13),s("cssClass","IngMultimedia"))},dependencies:[m,f],styles:[`body[_ngcontent-%COMP%] {
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+      background-color: #f8f9fa;
+      color: #1e1e1e;
+      margin: 0;
+      padding: 0;
+      transition: background-color 0.3s ease;
+    }
+
+    .container[_ngcontent-%COMP%] {
+      max-width: 1200px;
+      margin: 0 auto;
+      padding: 16px;
+    }
+
+    .section[_ngcontent-%COMP%] {
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+      gap: 16px;
+      transition: grid-template-columns 0.3s ease;
+    }
+
+    .card[_ngcontent-%COMP%] {
+      background-color: #ffffff;
+      border-radius: 12px;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+      padding: 20px;
+      transition: transform 0.3s ease, box-shadow 0.3s ease;
+      cursor: pointer;
+    }
+
+    .card[_ngcontent-%COMP%]:hover {
+      transform: scale(1.05);
+      box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+    }
+
+    .card-content[_ngcontent-%COMP%] {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      height: 100%;
+    }
+
+    @media (max-width: 767px) {
+      .card[_ngcontent-%COMP%] {
+        padding: 16px;
+      }
+    }`]});let a=i;return a})();export{C as default};
