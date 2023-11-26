b80c676bf0cf51d0600ad335d210e3494b972e22
multimediav22 <42384264+multimediav22@users.noreply.github.com>
2023-11-26 01:21:02 -0500

    Nueva Actualizacion Arreglo

---
+++ docs/chunk-2TF2QFSA.js
@@ -0,0 +1,65 @@
+import{Ba as c,Ka as n,La as a,Ma as r,Pa as o,S as l,Va as m,Wa as g,sa as d,ub as f,wb as p}from"./chunk-4POKWDE7.js";var y=t=>["w-full","h-[1000px]","rounded-md","overflow-hidden","bg-gray-200","transition-all","hover:bg-gray-300",t],x=(()=>{let e=class e{constructor(){let s=Date.now();for(;Date.now()-s<1e3;)console.log("Cargado")}};e.\u0275fac=function(i){return new(i||e)},e.\u0275cmp=l({type:e,selectors:[["app-heavy-loaders-slows"]],inputs:{cssClass:"cssClass"},standalone:!0,features:[m],decls:23,vars:3,consts:[["rel","stylesheet","href","https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css","integrity","sha512-....","crossorigin","anonymous"],[3,"ngClass"],[1,"p-6","relative","flex","flex-col","lg:flex-row","h-auto"],[1,"w-full","lg:w-2/3","pr-0","lg:pr-6","overflow-y-auto"],[1,"absolute","inset-0","bg-gradient-to-r","from-gray-200","via-gray-300","to-gray-400","rounded-md","filter","blur-md"],[1,"relative","z-10"],[1,"bg-zinc-800","text-white","p-4","mb-4"],[1,"container","mx-auto"],[1,"text-2xl","md:text-3xl","lg:text-4xl","font-semibold","mb-4","text-gray-400","leading-loose"],[1,"text-2xl","md:text-3xl","lg:text-4xl","font-semibold","mb-4","text-gray-800","leading-loose"],[1,"my-2","border-t","border-gray-400"],[1,"bg-black","rounded-md","p-4"],[1,"text-white","text-gray-600","text-sm"],[1,"text-white","text-gray-600","text-sm","ml-2","mr-4"],[1,"bg-white","bg-opacity-80","rounded-md","shadow-md","p-6","mb-6"],[1,"text-gray-800","leading-loose"],[1,"w-full","lg:w-1/3","mt-6","lg:mt-0"],["src","tu-imagen.jpg","alt","Imagen",1,"w-full","h-auto","rounded-md"]],template:function(i,u){i&1&&(r(0,"link",0),n(1,"section",1)(2,"div",2)(3,"div",3),r(4,"div",4),n(5,"div",5)(6,"nav",6)(7,"div",7)(8,"div",8),o(9," INGENIERO MULTIMEDIA "),a()()(),n(10,"h2",9),o(11," Educaci\xF3n "),r(12,"hr",10),n(13,"div",11)(14,"span",12),o(15,"Universidad Aut\xF3noma De Occidente"),a(),n(16,"span",13),o(17,"Enero 2015 - Octubre 2023"),a()()(),n(18,"div",14)(19,"p",15),o(20," Soy graduado en Ingenier\xEDa Multimedia de la Universidad Aut\xF3noma de Occidente en Cali, con s\xF3lidas habilidades en dise\xF1o, contenidos digitales, inform\xE1tica y sistemas multimedia interactivos. Mi pasi\xF3n por el crecimiento y el aprendizaje me impulsa a utilizar mis habilidades como una fuente constante de conocimiento y desarrollo. Estoy emocionado por aplicar esta experiencia para contribuir y aportar conocimientos en proyectos innovadores que mejoren la experiencia del usuario. Mi compromiso es aportar creatividad y habilidades t\xE9cnicas a un equipo din\xE1mico y colaborativo. "),a()()()(),n(21,"div",16),r(22,"img",17),a()()()),i&2&&(d(1),c("ngClass",g(1,y,u.cssClass)))},dependencies:[p,f],encapsulation:2});let t=e;return t})();var E=(()=>{let e=class e{};e.\u0275fac=function(i){return new(i||e)},e.\u0275cmp=l({type:e,selectors:[["ng-component"]],standalone:!0,features:[m],decls:17,vars:1,consts:[["lang","es"],["charset","UTF-8"],["name","viewport","content","width=device-width, initial-scale=1.0"],["rel","stylesheet","href","https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css","integrity","sha512-...","crossorigin","anonymous"],["rel","stylesheet","href","https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"],[1,"container"],[1,"section"],[1,"cards-container"],[1,"card","md:card-lg","lg:card-xl"],[1,"card-content"],[3,"cssClass"],[1,"card-description","mt-4"]],template:function(i,u){i&1&&(n(0,"html",0)(1,"head"),r(2,"meta",1)(3,"meta",2)(4,"link",3)(5,"link",4),n(6,"title"),o(7,"Tu T\xEDtulo"),a()(),n(8,"body")(9,"div",5)(10,"section",6)(11,"div",7)(12,"div",8)(13,"div",9),r(14,"app-heavy-loaders-slows",10),n(15,"p",11),o(16,"Acad\xE9mico"),a()()()()()()()()),i&2&&(d(14),c("cssClass","Ingeniero Multimedia"))},dependencies:[p,x],styles:[`body[_ngcontent-%COMP%] {
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+      background-color: #f8f9fa;
+      color: #1e1e1e;
+      margin: 0;
+      padding: 0;
+      transition: background-color 0.3s ease;
+    }
+
+    .container[_ngcontent-%COMP%] {
+      max-width: 1200px;
+      margin: 0 auto;
+      padding: 16px;
+    }
+
+    .section[_ngcontent-%COMP%] {
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+      gap: 16px;
+      transition: grid-template-columns 0.3s ease;
+    }
+
+    .cards-container[_ngcontent-%COMP%] {
+      max-height: 100%; 
+
+      overflow-y: auto;
+    }
+
+    .card[_ngcontent-%COMP%] {
+      background-color: #ffffff;
+      border-radius: 12px;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+      padding: 20px;
+      transition: transform 0.3s ease, box-shadow 0.3s ease;
+      cursor: pointer;
+    }
+
+    .card[_ngcontent-%COMP%]:hover {
+      transform: scale(1.05);
+      box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+    }
+
+    .card-content[_ngcontent-%COMP%] {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      height: 100%;
+    }
+
+    .card-title[_ngcontent-%COMP%] {
+      font-size: 1.5rem;
+      font-weight: 700;
+      margin-bottom: 10px;
+    }
+
+    .card-description[_ngcontent-%COMP%] {
+      font-size: 1rem;
+      color: #636363;
+    }
+
+    @media (max-width: 767px) {
+      .card[_ngcontent-%COMP%] {
+        padding: 16px;
+      }
+    }`]});let t=e;return t})();export{E as default};
